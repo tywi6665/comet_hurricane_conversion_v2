3d2d8202c9af4a592c0ebdf5c2ba2606c066a6d4
tywi6665 <tyletcole@gmail.com>
2020-06-08 12:26:31 -0600

    Populating Modal.js body

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
+import ReactDOM from "react-dom";
 
-const Modal = () => {
-    return (
-
-    );
-}
+const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
+    <>
+        <div className="modal-overlay" />
+        <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+            <div className="modal">
+                <div className="modal-header">
+                    <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <p>
+                    Hello, I'm a modal.
+        </p>
+            </div>
+        </div>
+    </>, document.body
+) : null;
 
 export default Modal;

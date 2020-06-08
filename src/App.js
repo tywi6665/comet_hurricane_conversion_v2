17bff2fbd1eab84a727543abcb5efa10a9285623
tywi6665 <tyletcole@gmail.com>
2020-06-08 12:35:18 -0600

    styling modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Container from "./Components/Containers/Container";
 import SubContainer from "./Components/Containers/SubContainer";
+import Modal from "./Components/Modal";
+import useModal from "./utils/useModal";
 import './App.scss';
 
 function App() {
+
+  const { isShowing, toggle } = useModal();
+
   return (
     <Container>
       <SubContainer>
         <h1>
           Aim a Hurricane
         </h1>
+      </SubContainer>
+      <SubContainer>
+        Stuff
+      </SubContainer>
+      <SubContainer>
+        <button className="button-default" onClick={toggle}>Show Modal</button>
+        <Modal
+          isShowing={isShowing}
+          hide={toggle}
+        />
       </SubContainer>
     </Container>
   );

6cd7f0d551a77ada9e3d54e098bf6c2adfcdafac
tywi6665 <tyletcole@gmail.com>
2020-06-08 11:46:18 -0600

    Created Components directory and Container component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Container from "./Components/Container";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <Container>
 
-    </div>
+    </Container>
   );
 }
 

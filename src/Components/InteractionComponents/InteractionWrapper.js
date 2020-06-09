fee70e2eed3f73cd33f902f2d23ea2f24c30bcac
tywi6665 <tyletcole@gmail.com>
2020-06-09 11:08:08 -0600

    Created dnd target divs

--- src/Components/InteractionComponents/InteractionWrapper.js
+++ src/Components/InteractionComponents/InteractionWrapper.js
@@ -7,15 +7,15 @@ const InteractionWrapper = () => {
                 <img src="./graphics/sample.jpg" alt="" />
             </div>
             <div className="drag-n-drop">
-                <div className="dnd-group">
+                <div className="dnd-group dnd-draggables-container">
                     <div className="dnd-item">
                         <div>
-                            <p>ITEM 1</p>
+                            <p>ITEM L</p>
                         </div>
                     </div>
                     <div className="dnd-item">
                         <div>
-                            <p>ITEM 2</p>
+                            <p>ITEM H</p>
                         </div>
                     </div>
                     <div className="dnd-item">
@@ -23,6 +23,26 @@ const InteractionWrapper = () => {
                             <p>ITEM 3</p>
                         </div>
                     </div>
+                </div>
+                <div className="dnd-group dnd-target dnd-target-low-1">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-low-2">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-low-3">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-low-4">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-high-1">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-high-2">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-high-3">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-high-4">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-hurricane-1">
+                </div>
+                <div className="dnd-group dnd-target dnd-target-hurricane-2">
                 </div>
             </div>
         </div>

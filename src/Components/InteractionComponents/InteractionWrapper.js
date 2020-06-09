f3194c0cdecfe9ed7be402f8331780782b96e3f0
tywi6665 <tyletcole@gmail.com>
2020-06-09 10:35:34 -0600

    Created shell for dnd interaction

--- src/Components/InteractionComponents/InteractionWrapper.js
+++ src/Components/InteractionComponents/InteractionWrapper.js
@@ -3,7 +3,18 @@ import React from 'react';
 const InteractionWrapper = () => {
     return (
         <div className="interaction-wrapper">
-
+            <div className="img-wrapper">
+                <img src="./graphics/sample.jpg" alt="" />
+            </div>
+            <div className="drag-n-drop">
+                <div className="dnd-group">
+                    <div className="dnd-item">
+                        <div>
+                            <p>Item</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
